4e96f763ddd2efa6c27cd0b085682ae62a72453c
susantharakan <susantharakan@myclubbetting.com>
2018-06-01 12:27:57 +0100

    Batch Service Application

---
+++ src/main/java/co/uk/mcb/clubwin/ClubWinBatchApplication.java
@@ -0,0 +1,15 @@
+package co.uk.mcb.clubwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubWinBatchApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ClubWinBatchApplication.class, args);
+    }
+
+}
+
+
